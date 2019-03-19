7acfbbcde8f346b0ba3cdc830afda3357113940b
sdim-ua <ingersol@yandex.ua>
2019-03-19 23:40:04 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,44 @@
+/* 
+Кто меня вызывал?
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        method1();
+        
+    }
+
+    public static int method1() {
+        method2();
+         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+   //      System.out.println(stackTraceElements[2].getLineNumber());
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method2() {
+        method3();
+         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+    //     System.out.println(stackTraceElements[2].getLineNumber());
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method3() {
+        method4();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+     //    System.out.println(stackTraceElements[2].getLineNumber());
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method4() {
+        method5();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+      //   System.out.println(stackTraceElements[2].getLineNumber());
+        return stackTraceElements[2].getLineNumber();
+    }
+
+    public static int method5() {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+       // System.out.println(stackTraceElements[2].getLineNumber());
+        return stackTraceElements[2].getLineNumber();
+    }
+}
